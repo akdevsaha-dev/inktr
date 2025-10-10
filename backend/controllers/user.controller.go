7b28d482f4b6371eb9fbd25ca399e4ec5a907596
Akdev Saha <akdevsaha2003@gmail.com>
2025-10-10 15:11:18 +0530

    feat: add signup and signin route

---
+++ backend/controllers/user.controller.go
@@ -0,0 +1,97 @@
+package controllers
+
+import (
+	"os"
+	"time"
+
+	"github.com/akdevsaha-dev/inktr-backend/config"
+	"github.com/akdevsaha-dev/inktr-backend/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var jwtSecret = []byte(os.Getenv("SECRET_KEY"))
+
+func Signup(c *fiber.Ctx) error {
+
+	var data struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	var exsitingUser models.User
+
+	if err := config.DB.Where("email = ?", data.Email).First(&exsitingUser).Error; err == nil {
+		return c.Status(400).JSON(fiber.Map{"error": "User with this email already exists!"})
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data.Password), 15)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Failed to hash password"})
+	}
+
+	user := models.User{
+		Name:     data.Name,
+		Email:    data.Email,
+		Password: string(hashedPassword),
+	}
+	err = config.DB.Create(&user).Error
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Cannot create user!"})
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"user_id": user.ID,
+			"exp":     time.Now().Add(time.Hour * 24).Unix(),
+		},
+	)
+	tokenString, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "failed to create token"})
+	}
+	return c.JSON(fiber.Map{
+		"message": "Signup successful",
+		"token":   tokenString,
+	})
+}
+
+func Singin(c *fiber.Ctx) error {
+	var data struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+	var existingUser models.User
+
+	if err := config.DB.Where("email = ? ", data.Email).First(&existingUser).Error; err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid Credentials"})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(data.Password)); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "invalid credentials."})
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"user_Id": existingUser.ID,
+			"exp":     time.Now().Add(time.Hour * 24).Unix(),
+		},
+	)
+	tokenString, err := token.SignedString(jwtSecret)
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": "Failed to create token",
+		})
+	}
+	return c.JSON(fiber.Map{"message": "signin successful",
+		"token": tokenString,
+	})
+}
---
+++ backend/routes/user.route.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/akdevsaha-dev/inktr-backend/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterUserRoutes(app *fiber.App) {
+	userGroup := app.Group("/api/v1/user")
+	userGroup.Post("/signup", controllers.Signup)
+	userGroup.Post("/signin", controllers.Singin)
+}
